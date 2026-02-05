32644bd611e60878bea4dac1870d833617b4eca0
MikhailSax <dominobryanskiy@yandex.ru>
2026-02-05 13:17:48 +0800

    Улучшить клиентский фронтенд карты и синхронизацию Vue с Symfony API

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,24 +2,25 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Advertisement;
+use App\Entity\AdvertisementBooking;
 use App\Entity\Banner;
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Attribute\AdminDashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
-use Symfony\Component\HttpFoundation\Response;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
-
+use Symfony\Component\HttpFoundation\Response;
 
 #[AdminDashboard(routePath: '/admin', routeName: 'admin')]
 class DashboardController extends AbstractDashboardController
 {
     public function index(): Response
     {
-
         $routeBuilder = $this->container->get(AdminUrlGenerator::class);
-        $url = $routeBuilder->setController(UserCrudController::class)->generateUrl();
+        $url = $routeBuilder->setController(AdvertisementBookingCrudController::class)->generateUrl();
+
         return $this->redirect($url);
     }
 
@@ -31,8 +32,11 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToRoute('Вернутся на сайт', 'fas fa-home', 'app_homepage');
+        yield MenuItem::linkToRoute('Вернуться на сайт', 'fas fa-home', 'app_homepage');
         yield MenuItem::linkToCrud('Пользователи', 'fas fa-user', User::class);
-        yield MenuItem::linkToCrud('Баннеры', 'fas fa-user', Banner::class);
+        yield MenuItem::linkToCrud('Баннеры', 'fas fa-image', Banner::class);
+        yield MenuItem::linkToCrud('Рекламные конструкции', 'fas fa-map-marker-alt', Advertisement::class);
+        yield MenuItem::linkToCrud('Бронирования', 'fas fa-calendar-check', AdvertisementBooking::class);
+        yield MenuItem::linkToRoute('Календарь занятости', 'fas fa-calendar', 'admin_booking_calendar');
     }
 }
